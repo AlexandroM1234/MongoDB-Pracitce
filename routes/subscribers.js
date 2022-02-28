a6ccb76386cf786816720336a2073268db2d8589
Alex Martinez <alexmartinez0071234@gmail.com>
2022-02-28 15:41:58 -0500

    CRUD routes made but not functional yet

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -3,13 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 // get all
-router.get("");
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 // get one
 
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
 // update one
+router.patch("/:id", (req, res) => {});
 
 // create one
+router.post("/", (req, res) => {});
 
 // delete one
+router.delete("/:id", (req, res) => {});
+
 module.exports = router;
